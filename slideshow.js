e067c69433752c6f2a77dce9ab60b4bdd64aba0c
True Ghiassi <true@ghiassitrio.co.uk>
2019-02-07 16:15:56 +0000

    It's mostly done, except for the reviews page.

---
+++ slideshow.js
@@ -0,0 +1,53 @@
+var curIndex = 0,
+    imgDuration = 4000,
+    slider = document.getElementById("slider"),
+    slides = slider.childNodes;
+    imgArray = [
+        'topslideshow/14kitchen2.jpg',
+        'topslideshow/07viewDownEstuary2.jpg',
+        'topslideshow/02loungeLookingIn.jpg',
+        'topslideshow/03viewDownEstuary.jpg',
+        'topslideshow/05estuaryFromLounge.jpg',
+        'topslideshow/06paddleBoardOnEstuary.jpg',
+        'topslideshow/08kitchen.jpg',
+        'topslideshow/09estuaryThroughBlind.jpg',
+        'topslideshow/10viewAcrossEstuary.jpg',
+        'topslideshow/01sconesOnBalcony.jpg',
+        'topslideshow/11loungeBlueSofa.jpg',
+        'topslideshow/12doubleBedroom.jpg',
+        'topslideshow/13twinBedroom.jpg']
+
+
+function buildSlideShow(arr) {
+    for (i = 0; i < arr.length; i++) {
+        var img = document.createElement('img');
+        img.className = "sliderimg";
+        img.src = arr[i];
+        slider.appendChild(img);
+    }
+}
+
+
+function slideShow() {
+    function fadeIn(e) {
+        e.className = "fadeIn";
+    };
+    function fadeOut(e) {
+        e.className = "";
+    };
+
+    fadeOut(slides[curIndex]);
+
+    curIndex++;
+    if (curIndex == slides.length) {
+        curIndex = 0;
+    }
+
+    fadeIn(slides[curIndex]);
+
+    setTimeout(slideShow, imgDuration);
+};
+
+
+buildSlideShow(imgArray);
+slideShow();
---
+++ testserver.py
@@ -0,0 +1,33 @@
+import bottle
+
+@bottle.get('/')
+def getmain():
+    return bottle.static_file('index.html', root='.')
+
+@bottle.get('/<filename>')
+def getstyles(filename):
+    if '.' not in filename:
+        filename = filename + '.html'
+    return bottle.static_file(filename, root='.')
+
+@bottle.get('/slideshow/<filename>')
+def slideshow(filename):
+    return bottle.static_file(filename, root='./slideshow')
+
+@bottle.get('/topslideshow/<filename>')
+def topslideshow(filename):
+    return bottle.static_file(filename, root='./topslideshow')
+
+@bottle.get('/reviewPhotos/<filename>')
+def topslideshow(filename):
+    return bottle.static_file(filename, root='./reviewPhotos')
+
+@bottle.get('/localPhotos/<filename>')
+def topslideshow(filename):
+    return bottle.static_file(filename, root='./localPhotos')
+
+@bottle.get('/roomPhotos/<filename>')
+def topslideshow(filename):
+    return bottle.static_file(filename, root='./roomPhotos')
+
+bottle.run(host='localhost', port=3000, debug=True)
